ec45cf6b5e8edfeac7a5b82588e55db6c3537edc
AgustinLaure <agustin.laure@estudiante.imagecampus.edu.ar>
2026-06-26 12:16:21 -0300

    Create Player Animation placeholder

---
+++ Assets/Script/PlayerAnimator.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    enum State
+    {
+        Idle,
+        Attack,
+        Walk,
+        Run,
+        Hurt,
+        Fall,
+        Jump,
+        Death
+    }
+
+    private State currentState = State.Idle;
+    private State prevState = State.Idle;
+    private void Update()
+    {
+        switch (currentState)
+        {
+            case State.Idle:
+
+                break;
+
+            case State.Attack:
+
+                break;
+
+            case State.Walk:
+
+                break;
+
+            case State.Run:
+
+                break;
+
+            case State.Hurt:
+
+                break;
+
+            case State.Fall:
+
+                break;
+
+            case State.Jump:
+
+                break;
+
+            case State.Death:
+
+                break;
+
+            default:
+                break;
+        }
+    }
+}
