c4dcc92cf7a2fe9c43b87b0f28e0532b75c01ad4
AgustinLaure <agustin.laure@estudiante.imagecampus.edu.ar>
2026-06-27 02:12:58 -0300

    Add IState interface

--- Assets/Script/PlayerAnimator.cs
+++ Assets/Script/PlayerAnimator.cs
@@ -2,60 +2,13 @@ using UnityEngine;
 
 public class PlayerAnimator : MonoBehaviour
 {
-    enum State
-    {
-        Idle,
-        Attack,
-        Walk,
-        Run,
-        Hurt,
-        Fall,
-        Jump,
-        Death
-    }
+    [SerializeField] private PlayerController controller;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Animator animator;
+    [SerializeField] private GameObject player;
 
-    [SerializeField] PlayerController playerController;
-
-    private State currentState = State.Idle;
-    private State prevState = State.Idle;
     private void Update()
     {
-        switch (currentState)
-        {
-            case State.Idle:
-
-                break;
-
-            case State.Attack:
-
-                break;
-
-            case State.Walk:
-
-                break;
-
-            case State.Run:
-
-                break;
-
-            case State.Hurt:
-
-                break;
-
-            case State.Fall:
-
-                break;
-
-            case State.Jump:
-
-                break;
-
-            case State.Death:
-
-                break;
-
-            default:
-                break;
-        }
+        
     }
 }
