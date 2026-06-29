8d1cb45f028c551835e86c42a68d8644f7a779be
Laure Agustin <agustin.laure@estudiante.imagecampus.edu.ar>
2026-06-29 12:41:38 -0300

    Try Add Enemy patrolling

--- Assets/Script/MeleeEnemy.cs
+++ Assets/Script/MeleeEnemy.cs
@@ -8,7 +8,15 @@ public class MeleeEnemy : Enemy
     [SerializeField] private GameObject attackArea;
     [SerializeField] private float attackCooldown;
 
+    [SerializeField] private AreaTrigger[] controlPoints;
+    [SerializeField] private float acceleration;
+    [SerializeField] private Vector2 terminalVelocity;
+    [SerializeField] private ForceMode2D forceMode;
+
+    private int currentControlPointIndex = 0;
+
     private AreaTrigger attackAreaTrigger;
+    private Rigidbody2D rb;
 
     private bool canAttack = true;
 
@@ -16,14 +24,50 @@ public class MeleeEnemy : Enemy
 
     private void Awake()
     {
-        attackArea.GetComponent<AreaTrigger>();
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        attackAreaTrigger = attackArea.GetComponent<AreaTrigger>();
+
+        foreach (AreaTrigger trigger in controlPoints)
+        {
+            trigger.OnTrigger += HandleControlPointCollision;
+        }
 
         attackAreaTrigger.OnTrigger += HandleAttackAreaCollision;
     }
 
+
     private void Update()
     {
         Attack();
+        Patrol();
+    }
+
+    private void Patrol()
+    {
+        Vector2 targetDir = (controlPoints[currentControlPointIndex].transform.position - transform.position).normalized;
+
+        rb.AddForce(new Vector2(targetDir.x * acceleration, 0f), forceMode);
+
+        if (rb.linearVelocityX > terminalVelocity.x)
+        {
+            rb.linearVelocityX = terminalVelocity.x;
+        }
+    }
+
+    private void ChangeControlPoint()
+    {
+        if (currentControlPointIndex + 1 < controlPoints.Length)
+        {
+            currentControlPointIndex++;
+        }
+        else
+        {
+            currentControlPointIndex = 0;
+        }
     }
 
     private void Attack()
@@ -36,6 +80,11 @@ public class MeleeEnemy : Enemy
     private void OnDestroy()
     {
         attackAreaTrigger.OnTrigger -= HandleAttackAreaCollision;
+
+        foreach (AreaTrigger trigger in controlPoints)
+        {
+            trigger.OnTrigger -= HandleControlPointCollision;
+        }
     }
 
     private void HandleAttackAreaCollision(Collider2D collider)
@@ -43,6 +92,14 @@ public class MeleeEnemy : Enemy
         if (collider.CompareTag("Player"))
         {
             collider.GetComponent<Player>().GetHealthPoints.TakeDamage(damage);
+        }
+    }
+
+    private void HandleControlPointCollision(Collider2D collider)
+    {
+        if (collider.CompareTag("Enemy"))
+        {
+            ChangeControlPoint();
         }
     }
 }
